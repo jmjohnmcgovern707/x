5a3e148b5e521c347a1c0f005ef3f07bce2e54f7
ttjohninvest@gmail.com <ttjohninvest@gmail.com>
2021-09-28 06:02:14 -0700

    x.com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-        
+    <div className="App"> 
         <p>
          Chat Social Media Subjects Index
         </p>
@@ -17,25 +14,7 @@ function App() {
         >
           chatscroll for Jesus Christ
         </a>
-        <a
-        className="App-link"
-        href="https://chatscroll.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          chatscroll
-        </a>
-        <a
-        className="App-link"
-        href="https://sports.chatscroll.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          chatscroll for sports
-        </a>
-        
        
-      
     </div>
   );
 }
