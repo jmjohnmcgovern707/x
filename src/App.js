b6ef6c7e0bc8f159c5b7ae6e0c243f1883561c95
ttjohninvest@gmail.com <ttjohninvest@gmail.com>
2021-09-28 00:38:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ function App() {
         </p>
         <a
         className="App-link"
+        href="https://chatscroll.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          chatscroll
+        </a>
+        <a
+        className="App-link"
         href="https://sports.chatscroll.com"
         target="_blank"
         rel="noopener noreferrer"
